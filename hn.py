ee09251ecf25a5e34d82d4014d90d004a9c32d14
gaureeshk <80737740+gaureeshk@users.noreply.github.com>
2022-09-25 11:51:31 +0530

    Update hn.py

--- hn.py
+++ hn.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = np.arange(10)
+n = np.arange(10000)
 fn=(-1/2)**n
 hn1=np.pad(fn, (0,2), 'constant', constant_values=(0))
 hn2=np.pad(fn, (2,0), 'constant', constant_values=(0))
